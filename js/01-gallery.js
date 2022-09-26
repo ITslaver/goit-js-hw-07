c8216a82c7ad0614a2c4ce9c1222bb60bd976554
Viacheslav Polishchuk <mr_v.p@ukr.net>
2022-09-27 00:27:37 +0300

    fix link library

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,56 +1,58 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+console.log(galleryItems);
+
 function renderMarkup(items) {
-    return items
-      .map(({ preview, original, description }) => {
-        return `<div class="gallery__item">
-    <a class="gallery__link" href="${original}">
-      <img
-        class="gallery__image"
-        src="${preview}"
-        data-source="${original}"
-        alt="${description}"
-      />
-    </a>
-  </div>`;
-      })
-      .join("");
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMakrup = renderMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMakrup);
+
+galleryContainer.addEventListener("click", onClickGetModal);
+
+function onClickGetModal(evt) {
+  evt.preventDefault();
+
+  const isImage = evt.target.classList.contains("gallery__image");
+
+  if (!isImage) {
+    return;
   }
-  
-  const galleryContainer = document.querySelector(".gallery");
-  const galleryMakrup = renderMarkup(galleryItems);
-  galleryContainer.insertAdjacentHTML("beforeend", galleryMakrup);
-  
-  galleryContainer.addEventListener("click", onClickGetModal);
-  
-  function onClickGetModal(evt) {
-    evt.preventDefault();
-  
-    const isImage = evt.target.classList.contains("gallery__image");
-  
-    if (!isImage) {
-      return;
-    }
-    const currentImage = evt.target;
-  
-    const instance = basicLightbox.create(`
-          <img src="${currentImage.dataset.source}" alt="${currentImage["alt"]}" >
-          `);
-    instance.show();
-  
-    document.addEventListener("keydown", onCloseModal);
-  
-    function onCloseModal(evt) {
-      // console.log(evt.key);
-      if (evt.key === "Escape") {
-        instance.close();
-        document.removeEventListener("keydown", onCloseModal);
-      }
+  const currentImage = evt.target;
+
+  const instance = basicLightbox.create(`
+        <img src="${currentImage.dataset.source}" alt="${currentImage["alt"]}" >
+        `);
+  instance.show();
+
+  document.addEventListener("keydown", onCloseModal);
+
+  function onCloseModal(evt) {
+    // console.log(evt.key);
+    if (evt.key === "Escape") {
+      instance.close();
+      document.removeEventListener("keydown", onCloseModal);
     }
   }
+}
   
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 
